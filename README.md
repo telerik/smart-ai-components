82059caf35b543aed7ae0323b191f297336291f7
Hristo Merdjanov <icom85@hotmail.com>
2024-05-16 16:50:33 +0300

    Add WinForms Smart AI demos (#3)
    * Add WinForms Smart AI demos
    - RichTextEditor
    - Spreadsheet
    
    * Update README with WinForms info

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # telerik-smartai-components
 
-Telerik Smart(AI) Components make it easy to add AI-powered features to your .NET applications without extra effort. Smart(AI) Components are currently in an **experimental phase** and are available for [Telerik UI for Blazor](https://www.telerik.com/blazor-ui), [Telerik UI for ASP.NET Core](https://www.telerik.com/aspnet-core-ui), and [Telerik UI for WPF](https://www.telerik.com/products/wpf/overview.aspx).
+Telerik Smart(AI) Components make it easy to add AI-powered features to your .NET applications without extra effort. Smart(AI) Components are currently in an **experimental phase** and are available for [Telerik UI for Blazor](https://www.telerik.com/blazor-ui), [Telerik UI for ASP.NET Core](https://www.telerik.com/aspnet-core-ui), [Telerik UI for WPF](https://www.telerik.com/products/wpf/overview.aspx), and [Telerik UI for WinForms](https://www.telerik.com/products/winforms.aspx).
 
 ### 🚀 Your feedback matters
 We are looking to expand the use cases for AI-powered components and highly value your input on their usefulness and potential applications. Please share your thoughts and suggestions at the following link: [Insert Link Here].
@@ -9,6 +9,7 @@ The source code for the experiments is currently available in the repository und
 - [Telerik UI for Blazor](https://github.com/telerik/telerik-smartai-components/tree/master/blazor)
 - [Telerik UI for ASP.NET Core](https://github.com/telerik/telerik-smartai-components/tree/master/core)
 - [Telerik UI for WPF](https://github.com/telerik/telerik-smartai-components/tree/master/wpf)
+- [Telerik UI for WinForms](https://github.com/telerik/telerik-smartai-components/tree/master/winforms)
 
 You can also check out our demos on telerik.com:
 
@@ -24,6 +25,9 @@ You can also check out our demos on telerik.com:
 
 ### Telerik UI for WPF:
 - [Smart AI Components](https://docs.telerik.com/devtools/wpf/wpflab/smart-ai-components)
+
+### Telerik UI for WinForms:
+- [Smart AI Components](https://docs.telerik.com/devtools/winforms/winformslab/smart-ai-components)
 
 ## Contribution
 Issues and Pull Requests are welcome.
---
+++ winforms/RichTextEditor/SmartRichTextEditor/MainForm.cs
@@ -0,0 +1,142 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using SmartComponents.LocalEmbeddings;
+using System.Text;
+using Telerik.WinControls.UI;
+using Telerik.WinForms.Documents.FormatProviders.OpenXml.Docx;
+using Telerik.WinForms.Documents.Model;
+using Telerik.WinForms.Documents.RichTextBoxCommands;
+using Timer = System.Windows.Forms.Timer;
+
+namespace SmartAIComponents
+{
+    public partial class MainForm : RadForm
+    {
+        private Timer timer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            DocxFormatProvider provider = new DocxFormatProvider();
+            this.radRichTextSmartEditor.Document = provider.Import(File.ReadAllBytes(@"..\..\..\SampleData\New_App_specification.docx"));
+
+            this.radRichTextSmartEditor.RichTextBoxElement.PreviewEditorKeyDown += this.RichTextBoxElement_PreviewEditorKeyDown;
+            this.radRichTextSmartEditor.CommandExecuted += this.RadRichTextEditor_CommandExecuted;
+
+            this.timer = new Timer();
+            this.timer.Interval = 2000;
+            this.timer.Tick += Timer_Tick;
+
+            AllTextContent = File.ReadAllText(@"..\..\..\Context.txt");
+            string[] chunks = AllTextContent.Split("--- NEW PAGE ---", StringSplitOptions.RemoveEmptyEntries);
+
+            LocalEmbedder embedder = new LocalEmbedder();
+            PageEmbeddings = chunks.Select(x => KeyValuePair.Create(x, embedder.Embed(x))).ToDictionary(k => k.Key, v => v.Value);
+        }
+
+        public Dictionary<string, EmbeddingF32> PageEmbeddings { get; set; }
+
+        public string AllTextContent { get; set; }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            timer.Stop();
+
+            string question = this.GetCurrentText();
+            if (string.IsNullOrEmpty(question))
+            {
+                return;
+            }
+
+            string answer = this.AnswerQuestion(question);
+            AppendText(this.radRichTextSmartEditor, answer);
+        }
+
+        private string GetCurrentText()
+        {
+            Paragraph paragraph = this.radRichTextSmartEditor.Document.CaretPosition.GetCurrentParagraph();
+            StringBuilder sb = new StringBuilder();
+
+            if (paragraph != null)
+            {
+                foreach (Span span in paragraph.EnumerateChildrenOfType<Span>())
+                {
+                    sb.Append(span.Text);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private string AnswerQuestion(string question)
+        {
+            LocalEmbedder embedder = new LocalEmbedder();
+            EmbeddingF32 questionEmbedding = embedder.Embed(question);
+
+            string[] results = LocalEmbedder.FindClosest(questionEmbedding, PageEmbeddings.Select(x => (x.Key, x.Value)), 2);
+
+            string answer = CallOpenAIApi("You are a helpful assistant. Use the provided context to answer the user question. Context: " + string.Join(" --- ", results), question);
+
+            return answer;
+        }
+
+        private static string CallOpenAIApi(string systemPrompt, string message)
+        {
+            // Add your key and endpoint  to use the OpenAI API.
+            OpenAIClient client = new OpenAIClient(
+                new Uri("AZURE_ENDPOINT"),
+                new AzureKeyCredential("AZURE_KEY"));
+
+            ChatCompletionsOptions chatCompletionsOptions = new ChatCompletionsOptions()
+            {
+                DeploymentName = "DeploymentName",
+                Messages =
+                {
+                    new ChatRequestSystemMessage(systemPrompt),
+                    new ChatRequestUserMessage(message),
+                }
+            };
+
+            Response<ChatCompletions> response = client.GetChatCompletions(chatCompletionsOptions);
+            ChatResponseMessage responseMessage = response.Value.Choices[0].Message;
+
+            return responseMessage.Content;
+        }
+
+        private static void AppendText(RadRichTextEditor editor, string text)
+        {
+            Span s = new Span(text);
+            s.ForeColor = Color.DimGray;
+
+            editor.InsertInline(s);
+            editor.Document.CaretPosition.MoveToDocumentElementStart(s);
+        }
+
+        private void RadRichTextEditor_CommandExecuted(object? sender, CommandExecutedEventArgs e)
+        {
+            if (e.Command is InsertTextCommand)
+            {
+                this.timer.Stop();
+                this.timer.Start();
+            }
+        }
+
+        private void RichTextBoxElement_PreviewEditorKeyDown(object sender, Telerik.WinForms.Documents.PreviewEditorKeyEventArgs e)
+        {
+            if (e.OriginalArgs.KeyCode == Keys.Tab)
+            {
+                var p = this.radRichTextSmartEditor.Document.CaretPosition.GetCurrentParagraph();
+                var span = p.Inlines.OfType<Span>().Where(s => Color.DimGray == s.ForeColor).LastOrDefault();
+                if (span != null)
+                {
+                    e.SuppressDefaultAction = true;
+
+                    span.ForeColor = Color.Black;
+                    this.radRichTextSmartEditor.UpdateEditorLayout();
+                    this.radRichTextSmartEditor.Document.CaretPosition.MoveToDocumentElementEnd(p);
+                }
+            }
+        }
+    }
+}
